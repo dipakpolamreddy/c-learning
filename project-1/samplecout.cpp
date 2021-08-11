53b436892b0ad1c80cade4d4ef39aeb60a313392
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-11 11:54:06 +0530

    Create samplecout.cpp

---
+++ project-1/samplecout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int slices;  //declartion
+    slices = 5+1 ;     //initilization
+
+  
+
+    std::cout << "you have " << slices << "slices of pizza " << endl; 
+
+    return 0;
+}
+
+
