b91eae3fcb8d6c64210e01af09641fadbcf7b266
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-11 12:02:36 +0530

    Create userinput.cpp

---
+++ project-1/userinput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int slices;
+    cout << "how many slices of pizza u have : ";
+    cin >> slices; //user input from the console
+    std::cout << "i have " << slices << " slices of pizza";
+
+    return 0;
+}
+
