e7f7ecbdf4449c055238fde65ea83755b633605f
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-12 15:11:05 +0530

    Update stringmodifiers.cpp

--- project-1/stringmodifiers.cpp
+++ project-1/stringmodifiers.cpp
@@ -4,29 +4,33 @@
 int main()
 {
 
-    std::string greeting = "hello";
-    greeting += "there";                                   //concatinating the "there"
+    std::string greeting = "hello"; 
+    greeting += "there";                                                              //concatinating the "there"
     std::cout << greeting << std::endl;
 
     std::string tell = "hello";
-    greeting.append("world");                              //adding the string
+    greeting.append("world");                                                    //adding the string
     std::cout << tell << std::endl;
 
     std::string message = "hello";
-    message.insert(3, " ");                                 //insert space to the string
-    std::cout << message << std::endl;
+    message.insert(3, " ");                                                 //insert space to the string
+    std::cout << message << std::endl;      
 
     std::string msg = "hello";
     msg.erase(4);
-    std::cout << msg << std::endl;                      //   erasing the charace
+    std::cout << msg << std::endl;                                    //   erasing the charace
 
     std::string word = "hello";
-    word.pop_back();                                    //   erasing the charace
+    word.pop_back();                                               //   erasing the charace
     std::cout << word << std::endl;
 
     std::string reply= "hello";
-    reply.replace(2,4, "eaven");                        //   replacing the characters
+    reply.replace(2,4, "eaven");                             //   replacing the characters
     std::cout << reply  << std::endl;
+    
+    std::string sstr= "where is hell";
+    sstr.substr(5,2);                                   //   replacing the characters
+    std::cout << sstr  << std::endl;
 
 
 }
