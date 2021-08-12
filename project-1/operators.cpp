c1b58086a09eb57707ccd9f56701b9c11dcbf154
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-12 16:08:58 +0530

    Create operators.cpp

---
+++ project-1/operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int a = 5 + 5;
+    int b=  5 -5;
+    int c = 5 * 5;
+    int d = 5 % 5;
+    int e = 5 / 5;
+
+    std::cout << "a :" << a << std::endl;
+    std::cout << "b :" << b << std::endl;
+    std::cout << "c :" << c << std::endl;
+    std::cout << "d :" << d << std::endl;
+    std::cout << "e :" << e << std::endl;
+
+
+    double x = (10 + 4.0) / 3;   
+    std::cout << "x :" << x <<  std::endl;
+
+}
