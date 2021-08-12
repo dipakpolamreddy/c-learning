9245649e9005fe6a65067ce1a97da7c80000b865
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-12 15:01:40 +0530

    Create stringmodifiers.cpp

---
+++ project-1/stringmodifiers.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+
+    std::string greeting = "hello";
+    greeting += "there";                                   //concatinating the "there"
+    std::cout << greeting << std::endl;
+
+    std::string tell = "hello";
+    greeting.append("world");                              //adding the string
+    std::cout << tell << std::endl;
+
+    std::string message = "hello";
+    message.insert(3, " ");                                 //insert space to the string
+    std::cout << message << std::endl;
+
+    std::string msg = "hello";
+    msg.erase(4);
+    std::cout << msg << std::endl;                      //   erasing the charace
+
+    std::string word = "hello";
+    word.pop_back();                                    //   erasing the charace
+    std::cout << word << std::endl;
+
+    std::string reply= "hello";
+    reply.replace(2,4, "eaven");                        //   replacing the characters
+    std::cout << reply  << std::endl;
+
+
+}
