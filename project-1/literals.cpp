7d3d8dc888b80866ace1fc2787c269cfed7f608d
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-12 15:48:11 +0530

    Create literals.cpp

---
+++ project-1/literals.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x = 30;
+    std::cout << x << std:: endl;
+    std::cout <<std::hex << x << std::endl;            //value of 30 in hex is 1e where e represnts 14 + 1 time 16 =30
+    std::cout << std::oct << x << std::endl;           // value of 30 in octal is 36 where 6 is a number 3*8 is 24 = 30
+
+
+}
