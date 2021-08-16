10043ba53c785b15f2db83d9afcb32431bb0030d
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-16 19:41:18 +0530

    Create voidpointer.cpp

---
+++ project-1/voidpointer.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+    int a = 1025;
+    int* p;
+    p = &a;
+    std::cout << "address : " << *p << "   " << p << '\n';
+    std::cout << "address : " << *(p + 1) << "   " << p + 1 << '\n';
+
+    void * p0;                  //void pointer
+    p0 = p;
+
+    std::cout << "address : " << p0 << '\n';
+
+}
+ 
