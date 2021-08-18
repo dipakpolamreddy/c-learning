25a06a643613438ac66b96bb5214f378ad3b8b92
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-18 18:42:24 +0530

    Create cout.cpp

---
+++ project-3/cout.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";                                        //this will output to the console "helloworld"
+    std::cout << "Hello World!-2" <<std::endl;
+    std::cout << "Hello World-3\n" << std::endl;
+}
