bf2ee626668e0f18a9a9844b323d9b4ceb0bdd1d
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-19 12:29:01 +0530

    Create Ascii.cpp

---
+++ project-3/Ascii.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    char c1, c2, c3, c4, c5;
+    std::cout << "enter the 5 numbers:" << std::endl;
+    std::cin >> c1 >> c2 >> c3 >> c4 >> c5;
+    std::cout << "ASCII Message is ::" <<(int)c1 <<" "<< (int)c2 << " " << (int)c3 << " " << (int)c4 << " " << (int)c5 << " " << std::endl;
+}
