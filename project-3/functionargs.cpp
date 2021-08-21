0bd62e423cbb08a3efd10da92b3bbbeec5f45e53
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-21 17:45:24 +0530

    Create functionargs.cpp

---
+++ project-3/functionargs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void function(std::string name, std::string city, int age) {
+
+    std::cout << "the name is :" << name << std::endl;
+    std::cout << "the name is :" << city << std::endl;
+    std::cout << "the name is :" << age << std::endl;
+
+}
+
+int main()
+
+{
+
+    function("arjun", "delhi", 28);
+    std::cout << "Hello World!\n";
+}
