4ea312eb53457bd6affdc1a9ae198690fc427771
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-26 15:05:11 +0530

    Create userinput.cpp

---
+++ project-4/userinput.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+
+
+int main()
+{
+    int age;
+    std::cout << "enter the age :" << std::endl;
+    std::cin >> age;
+    std::cout << "your age is  "<< age;
+
+  
+}
