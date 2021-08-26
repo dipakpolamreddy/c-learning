5092a64403786338ef4323e3afff6b48d09da9e5
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-26 15:08:54 +0530

    Create function.cpp

---
+++ project-4/function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+void hi(std::string name, int age) {
+    std::cout << "his name is " << name << " and age is " << age << std::endl;
+}
+int main()
+{
+
+    hi("mike", 34);
+    
+}
