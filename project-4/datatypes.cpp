9a3f52b43fa9d91d40a5037f8db2a90cb76575f3
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-26 15:09:46 +0530

    Create datatypes.cpp

---
+++ project-4/datatypes.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    char c = 'E';
+    std::string name = "arjun";
+    float t = 2.3;
+    double g = 45.5;
+    bool ismale = false;
+    std::cout << a << endl;
+    std::cout << c << endl;
+    std::cout <<name << endl;
+    std::cout << t << endl;
+    std::cout << g << endl;
+}
+
