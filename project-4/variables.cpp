1893a8e35da899b0b79ea1b8d913d27e4b7076ac
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-26 15:05:36 +0530

    Create variables.cpp

---
+++ project-4/variables.cpp
@@ -0,0 +1,10 @@
+int main()
+
+{
+    string name = "john";
+    int age=25;
+    
+    std::cout << " my name is  \n"<< name <<endl;
+    std::cout << " my age is   \n" << age << endl;
+    std::cout << " he liked the name    \n" << name << endl;
+}
