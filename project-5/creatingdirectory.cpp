fe510b903a20c289b4fba6c43b0c6a3680b0d27f
deepak polamreddy <41968693+dipakpolamreddy@users.noreply.github.com>
2021-08-27 10:39:25 +0530

    Create creatingdirectory.cpp

---
+++ project-5/creatingdirectory.cpp
@@ -0,0 +1,19 @@
+#include<Windows.h>
+#include <iostream>
+
+int main()
+{
+	BOOL CDir;                                                                          /*creating directory function structure */
+	CDir=CreateDirectory(
+		L"C:\\deepak",
+		NULL
+	);
+	if (CDir == false) {
+		std::cout << " create directory Failed" << GetLastError() << std::endl;
+		
+	}
+	else {
+		std::cout << "Creating Directory Success" << std::endl;
+	}
+	system("PAUSE");
+}
